bcf6a527ee4eec27693852d4d3848638a4f268f3
wangqing <wangqing@kaolafm.com>
2016-12-02 14:59:46 +0800

    1. 添加 log 的注释 0.0.3；

--- utilslibrary/src/main/java/team/zhuoke/utilslibrary/Log.java
+++ utilslibrary/src/main/java/team/zhuoke/utilslibrary/Log.java
@@ -1,14 +1,24 @@
 package team.zhuoke.utilslibrary;
 
 /**
+ * 这是我的工具类，测试使用
+ *
  * Created by WangQing on 2016/12/2.
  */
 
 public class Log {
 
+    /**
+     * log 关键字
+     */
     private static final String TAG = "Log";
 
+    /**
+     * 写一个文档注释
+     * @param text
+     */
     public static void wangqing(String text) {
-        android.util.Log.i(TAG, "wangqing: log");
+        // 写一个 注释而已，没别的
+        android.util.Log.i(TAG, "wangqing-》log：" + text);
     }
 }
