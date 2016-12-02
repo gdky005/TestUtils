b82cb59ae238d9e1c937b62a22faa5ada184a70f
wangqing <wangqing@kaolafm.com>
2016-12-02 11:33:37 +0800

    1. 添加 log 测试。

---
+++ utilslibrary/src/main/java/team/zhuoke/utilslibrary/Log.java
@@ -0,0 +1,14 @@
+package team.zhuoke.utilslibrary;
+
+/**
+ * Created by WangQing on 2016/12/2.
+ */
+
+public class Log {
+
+    private static final String TAG = "Log";
+
+    public static void wangqing(String text) {
+        android.util.Log.i(TAG, "wangqing: log");
+    }
+}
